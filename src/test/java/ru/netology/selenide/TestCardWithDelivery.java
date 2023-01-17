5b98de25db3cc2f2b06921ad134dda6a511ee0c1
Liliya Yapparova <yapparovalili@yandex.ru>
2023-01-17 15:08:23 +0700

    Changed test

---
+++ src/test/java/ru/netology/selenide/TestCardWithDelivery.java
@@ -0,0 +1,38 @@
+package ru.netology.selenide;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestCardWithDelivery {
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+
+    }
+
+    @Test
+    public void shouldSendForm() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Томск");
+        String currentDate = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $("[data-test-id='date'] input").sendKeys(currentDate);
+        $("[data-test-id='name'] input").setValue("Петрова Анна-Виктория");
+        $("[data-test-id='phone'] input").setValue("+79138489990");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на" + currentDate));
+
+
+    }
+
+}
